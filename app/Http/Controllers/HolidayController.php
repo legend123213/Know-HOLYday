3a99c02fe217f4d5eb82f40564d4b1f29c3c97d0
legend123213 <abelwendmu124@gmail.com>
2025-01-02 09:14:47 +0300

    add holiday-related methods to HolidayController and create card and navbar components

--- app/Http/Controllers/HolidayController.php
+++ app/Http/Controllers/HolidayController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class HolidayController extends Controller
@@ -10,4 +10,50 @@ class HolidayController extends Controller
     {
         return "home";
     }
+    public function getCountryNames()
+    {
+        $client = new Client();
+        $response = $client->get('https://date.nager.at/api/v3/AvailableCountries');
+        return $response->getBody();
+    }
+
+    public function getCountryHolidaysWithCY($countryCode, $year)
+    {
+        $client = new Client();
+        $response = $client->get("https://date.nager.at/api/v3/PublicHolidays/$year/$countryCode");
+        return $response->getBody();
+    }
+
+    public function isHoliday($countryCode, $date)
+    {
+        $client = new Client();
+        $response = $client->get("https://date.nager.at/api/v3/IsTodayPublicHoliday/$countryCode?date=$date");
+        return $response->getBody();
+    }
+
+
+
+    public function getNext7DaysHoliday($countryCode)
+    {
+        $client = new Client();
+        $response = $client->get("https://date.nager.at/api/v3/NextPublicHolidaysWorldwide");
+        $holidays = json_decode($response->getBody(), true);
+        $next7DaysHolidays = array_filter($holidays, function ($holiday) use ($countryCode) {
+
+            $country = $holiday['countryCode'];
+            return $country == $countryCode;
+        });
+        $holidays = $next7DaysHolidays;
+        return view('home', ['holidays' => $holidays]);
+    }
+    public function getNext7DaysHolidays()
+    {
+        $client = new Client();
+        $response = $client->get("https://date.nager.at/api/v3/NextPublicHolidaysWorldwide");
+        $holidays = json_decode($response->getBody(), true);
+        return view('home', ['holidays' => $holidays]);
+    }
+
+
+
 }
