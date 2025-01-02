25c3045d4eb5804f17ab745328090c5b6cebc610
legend123213 <abelwendmu124@gmail.com>
2025-01-02 10:03:00 +0300

    add filter component with selection forms for country and number input

---
+++ resources/views/components/filter.blade.php
@@ -0,0 +1,34 @@
+<div class="flex items-center justify-center ">
+    <div class="flex space-x-4">
+        <div>
+            <form class="max-w-sm mx-auto">
+                <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an
+                    option</label>
+                <select id="countries"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                    <option selected>Choose a country</option>
+                    <option value="US">United States</option>
+                    <option value="CA">Canada</option>
+                    <option value="FR">France</option>
+                    <option value="DE">Germany</option>
+                </select>
+            </form>
+        </div>
+
+        <div>
+            <form class="max-w-sm mx-auto">
+                <label for="number-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a
+                    number:</label>
+                <input type="number" id="number-input" aria-describedby="helper-text-explanation"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="90210" required />
+            </form>
+
+        </div>
+        <div class="">
+            <button type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+        </div>
+    </div>
+</div>
