01ab9f322a2875bd29546635c9f4c57b1944666b
legend123213 <abelwendmu124@gmail.com>
2025-01-03 02:52:41 +0300

    update navbar links to point to correct routes for holidays and upcoming days

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,13 +1,13 @@
 <div class="bg-gray-900 h-16 w-full">
     <ul class="flex p-5">
     <li class="mr-6">
-        <a class="text-white hover:text-blue-800" href="#">Holidays</a>
+        <a class="text-white hover:text-blue-800" href="/home">Holidays</a>
     </li>
     <li class="mr-6">
-        <a class="text-white hover:text-blue-800" href="#">Next 7 days</a>
+        <a class="text-white hover:text-blue-800" href="/next7days">Next 7 days</a>
     </li>
     <li class="mr-6">
-        <a class="text-white hover:text-blue-800" href="#">Next 365 days</a>
+        <a class="text-white hover:text-blue-800" href="/next365days">Next 365 days</a>
     </li>
     
 </ul>
