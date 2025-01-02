3f08555bafea44a62509c4fe7356b1c82545e9ab
legend123213 <abelwendmu124@gmail.com>
2025-01-03 01:40:06 +0300

    refactor routes to simplify controller usage and add new holiday endpoints

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,10 @@
 use App\Http\Controllers\HolidayController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $hello = new HolidayController;
-    return $hello->index();
-});
+Route::get('/', [HolidayController::class, 'index']);
 Route::get("/countrynames", [HolidayController::class, 'getCountryNames']);
 Route::get('/getNext7DaysHolidays/{id}', [HolidayController::class, 'getNext7DaysHoliday']);
-Route::get('/getNext7DaysHolidays', [HolidayController::class, 'getNext7DaysHolidays']);
+Route::get('/next7days', [HolidayController::class, 'getNext7DaysHolidays']);
 Route::get('/getHolidays', [HolidayController::class, 'getNext7DaysHolidays']);
+Route::get('/getCountryHolidaysWithCY', [HolidayController::class, 'getCountryHolidaysWithCY'])->name('getCountryHolidaysWithCY.form');
+Route::get('/next7daysWithC', [HolidayController::class, 'getNext7DaysHolidayWC'])->name('next7daysWithC.form');
