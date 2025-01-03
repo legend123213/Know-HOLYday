c24432e4bc971c37bdd1204261192dd2c1cb6450
legend123213 <abelwendmu124@gmail.com>
2025-01-03 13:28:46 +0300

    refactor(routes): update holiday routes for consistency and clarity

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\HolidayController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HolidayController::class, 'index']);
-// Route::get("/countrynames", [HolidayController::class, 'getCountryNames']);
-Route::get('/getNext7DaysHolidays/{id}', [HolidayController::class, 'getNext7DaysHoliday']);
-Route::get('/next7days', [HolidayController::class, 'getNext7DaysHolidays']);
-Route::get('/getHolidays', [HolidayController::class, 'getNext7DaysHolidays']);
-Route::get('/next365days', [HolidayController::class, 'getNextDaysHolidays']);
 
 
-Route::get('/getCountryHolidaysWithCY', [HolidayController::class, 'getCountryHolidaysWithCY'])->name('getCountryHolidaysWithCY.form');
-Route::get('/next7daysWithC', [HolidayController::class, 'getNext7DaysHolidayWC'])->name('next7daysWithC.form');
-Route::get('/getNext1YearHoliday', [HolidayController::class, 'getNext1YearHoliday'])->name('getNext1YearHoliday.form');
+Route::get('/holidays/next7days', [HolidayController::class, 'getNext7DaysHolidays']);
+Route::get('/holidays', [HolidayController::class, 'getNext7DaysHolidays']);
+Route::get('/holidays/next365days', [HolidayController::class, 'getNextDaysHolidays']);
+
+
+Route::get('/holidays/country', [HolidayController::class, 'getCountryHolidaysWithCY'])->name('getCountryHolidaysWithCY.form');
+Route::get('/holidays/next7days/country', [HolidayController::class, 'getNext7DaysHolidayWC'])->name('next7daysWithC.form');
+Route::get('/holidays/next1year', [HolidayController::class, 'getNext1YearHoliday'])->name('getNext1YearHoliday.form');
