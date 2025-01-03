0046783bea698ceca565efa700d02e6c45134556
legend123213 <abelwendmu124@gmail.com>
2025-01-03 12:22:20 +0300

    fix(navbar): update Holidays link to point to the root route

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-gray-900 h-16 w-full">
     <ul class="flex p-5">
     <li class="mr-6">
-        <a class="text-white hover:text-blue-800" href="/home">Holidays</a>
+        <a class="text-white hover:text-blue-800" href="/">Holidays</a>
     </li>
     <li class="mr-6">
         <a class="text-white hover:text-blue-800" href="/next7days">Next 7 days</a>
